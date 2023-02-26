52fc7bf421e6a58337c9d7d16f067a11add65f1a
Pierre Kisters <pierrekisters@gmail.com>
2023-02-26 15:14:51 +0100

    wt32-eth01

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,13 @@
 #include <esp_dmx.h>
 #include <ArtnetWifi.h>
 
-/*
-   * ETH_CLOCK_GPIO0_IN   - default: external clock from crystal oscillator
-   * ETH_CLOCK_GPIO0_OUT  - 50MHz clock from internal APLL output on GPIO0 - possibly an inverter is needed for LAN8720
-   * ETH_CLOCK_GPIO16_OUT - 50MHz clock from internal APLL output on GPIO16 - possibly an inverter is needed for LAN8720
-   * ETH_CLOCK_GPIO17_OUT - 50MHz clock from internal APLL inverted output on GPIO17 - tested with LAN8720
-*/
-#ifdef ETH_CLK_MODE
-#undef ETH_CLK_MODE
-#endif
-#define ETH_CLK_MODE    ETH_CLOCK_GPIO17_OUT
-
-// Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
-#define ETH_POWER_PIN   -1
-
-// Type of the Ethernet PHY (LAN8720 or TLK110)
-#define ETH_TYPE        ETH_PHY_LAN8720
-
-// I²C-address of Ethernet PHY (0 or 1 for LAN8720, 31 for TLK110)
-#define ETH_ADDR        1
-
-// Pin# of the I²C clock signal for the Ethernet PHY
-#define ETH_MDC_PIN     23
-
-// Pin# of the I²C IO signal for the Ethernet PHY
-#define ETH_MDIO_PIN    18
-
-const dmx_port_t dmx_num = DMX_NUM_2;
-
-const int dmx_tx_pin = 32;
-const int dmx_rx_pin = 33;
-const int dmx_rts_pin = 14;
+const dmx_port_t dmx_num = DMX_NUM_1;
+const int dmx_tx_pin = 15;
+const int dmx_rx_pin = 14;
+const int dmx_rts_pin = 4;
+const int dmx_universe_0_pin = 36;
+const int dmx_universe_1_pin = 39;
+const int dmx_universe_2_pin = 35;
 
 static bool eth_connected = false;
 
@@ -80,7 +56,12 @@ bool isConnected() {
 uint8_t dmxData[DMX_MAX_PACKET_SIZE];
 
 void onArtnetFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data) {
-    if (universe == 0) {
+    int dmx_universe =
+            digitalRead(dmx_universe_0_pin) |
+            digitalRead(dmx_universe_1_pin) << 1 |
+            digitalRead(dmx_universe_2_pin) << 2;
+
+    if (universe == dmx_universe) {
         dmxData[0] = 0;
         for (int i = 0; i < length && i < DMX_MAX_PACKET_SIZE - 1; i++) {
             dmxData[i + 1] = i < length ? data[i] : 0;
@@ -95,15 +76,23 @@ ArtnetWifi artnet;
 
 void setup() {
     Serial.begin(115200);
+    Serial.println("Initializing...");
+
     WiFi.onEvent(WiFiEvent);
-    ETH.begin(ETH_ADDR, ETH_POWER_PIN, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_TYPE, ETH_CLK_MODE);
+    ETH.begin();
+
     artnet.setArtDmxCallback(onArtnetFrame);
     artnet.begin();
+
     dmx_set_pin(dmx_num, dmx_tx_pin, dmx_rx_pin, dmx_rts_pin);
     dmx_driver_install(dmx_num, DMX_DEFAULT_INTR_FLAGS);
+
+    pinMode(dmx_universe_0_pin, INPUT);
+    pinMode(dmx_universe_1_pin, INPUT);
+    pinMode(dmx_universe_2_pin, INPUT);
+
     Serial.println("Initialized");
 }
-
 
 void loop() {
     if (isConnected()) {
